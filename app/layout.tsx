6fbee47a0a23a494b0b62d5ee44fc0b0839f3ec7
Adarsh Yadav <akankshaazm20@gmail.com>
2026-08-15 13:24:51 +0530

    feat: init nextjs, shadcn with themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,38 @@
-import { Geist, Geist_Mono, JetBrains_Mono, IBM_Plex_Sans } from "next/font/google"
+import {
+  Geist,
+  Geist_Mono,
+  JetBrains_Mono,
+  IBM_Plex_Sans,
+} from "next/font/google"
+import type { Metadata } from "next"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
-const ibmPlexSansHeading = IBM_Plex_Sans({subsets:['latin'],variable:'--font-heading'});
+const ibmPlexSansHeading = IBM_Plex_Sans({
+  subsets: ["latin"],
+  variable: "--font-heading",
+})
 
 const fontSans = Geist({
   subsets: ["latin"],
   variable: "--font-sans",
 })
 
-const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'})
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
+export const metadata: Metadata = {
+  title: "Battlefield",
+  description:
+    "Battlefield: a Next.js app that races open-source models side-by-side in real-time to track exact performance metrics like latency and cost. Vote on the winner to update a public leaderboard.",
+  icons: {
+    icon: "/logo.svg",
+  },
+}
 
 export default function RootLayout({
   children,
@@ -22,7 +43,13 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontSans.variable, "font-mono", jetbrainsMono.variable, ibmPlexSansHeading.variable)}
+      className={cn(
+        "antialiased",
+        fontSans.variable,
+        "font-mono",
+        jetbrainsMono.variable,
+        ibmPlexSansHeading.variable
+      )}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
